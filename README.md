f529bc39ad11887c7efd759e0e4812a932726e8a
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2012-10-27 17:40:41 +0200

    Maven build skeleton.

---
+++ README.md
@@ -0,0 +1,3 @@
+# VOMS 3.0
+
+This is the development branch for the Java based implementation of VOMS 3.0
---
+++ src/main/java/org/italiangrid/voms/service/Main.java
@@ -0,0 +1,13 @@
+package org.italiangrid.voms.service;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
